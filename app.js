5a6ee1b0657774d437f911c5afd717f97ef6fb75
Nicholas Mullikin <nicholas.mullikin@gmail.com>
2020-02-22 11:33:53 -0500

    Completely removed

--- app.js
+++ app.js
@@ -75,11 +75,7 @@ app.use(session({
   resave: true,
   saveUninitialized: true,
   secret: process.env.SESSION_SECRET,
-  cookie: { maxAge: 1209600000 }, // two weeks in milliseconds
-  store: new MongoStore({
-    url: process.env.MONGODB_URI,
-    autoReconnect: true,
-  })
+  cookie: { maxAge: 1209600000 } // two weeks in milliseconds
 }));
 app.use(passport.initialize());
 app.use(passport.session());
